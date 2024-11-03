c0f560ae85133c87dade952ecb9b094bb018bc14
x <thxxrxpxt.x@gmail.com>
2024-11-04 00:55:10 +0700

    add userData for profile

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const userController = require('../controllers/user.controller');
 const { verifyToken } = require('../middleware/auth.middleware');
 
+router.get('/userData/:user_id', verifyToken, userController.userData);
 router.get('/history/:user_id', verifyToken, userController.history);
 router.get('/dashboard', verifyToken, userController.summary);
 
