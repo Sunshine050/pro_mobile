564dbb5de5aa24162a4b91f3f90d3c2e6e49f3ae
x <thxxrxpxt.x@gmail.com>
2024-11-04 00:25:45 +0700

    move db to docker

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -2,8 +2,9 @@ const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
   host: 'localhost',
+  port: 3306,
   user: 'root',
-  password: '', 
+  password: 'root', 
   database: 'mobile_project'
 });
 
